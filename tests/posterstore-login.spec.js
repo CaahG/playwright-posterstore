edf4cc376eecc80e63870f7aa5a504abb50956e4
CaahG <caahg@YoungestAxes-MacBook-Air.local>
2024-12-05 19:13:48 -0300

    adicionando cenario de autenticacao de usuario * adicionando cenario de criacao de usuario

--- tests/posterstore-login.spec.js
+++ tests/posterstore-login.spec.js
@@ -5,7 +5,7 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('Login Spec', () => {
 
-    test('Invalid login', async ({ page }) => {
+    test('login invalido', async ({ page }) => {
         await page.goto('https://poster-store.pages.dev/#');
         await page.locator('a[id="login-button"]').click();
         await page.locator('input[id="swal-username"]').fill('user1');
@@ -14,7 +14,7 @@ test.describe('Login Spec', () => {
         await page.waitForSelector(':has-text("Invalid username or password")');
       });
       
-      test('Valid login and logout', async ({ page }) => {
+      test('login e logout invalido', async ({ page }) => {
         await page.goto('https://poster-store.pages.dev/#');
       
         await page.locator('a[id="login-button"]').click();
@@ -30,28 +30,5 @@ test.describe('Login Spec', () => {
 
 });
  
-//login invalido com sucesso , teste finalizado 
 
-       test('Criando User', async ({ page }) => {
-       await page.goto('https://poster-store.pages.dev/#');
-       await page.locator('a[class="btn btn-primary btn-sm"]').click();
-       await page.locator('input[id="swal-username"]').fill('lorelai');
-       await page.locator('input[id="swal-password"]').fill('gilmore1');
-      await page.locator('button[class="swal2-confirm swal2-styled"]').click();
-      await page.waitForSelector(':has-text("User created successfully")');
-      await page.locator('button[class="swal2-confirm swal2-styled"]').click();
-
-     });
-
-test('Criação de usuário com credenciais inválidas', async ({ page }) => {
-  await page.goto('https://poster-store.pages.dev/#');
-  await page.locator('a[class="btn btn-primary btn-sm"]').click();
-  await page.locator('input[id="swal-username"]').fill('jackesqueleto');
-  await page.locator('input[id="swal-password"]').fill('-');
-  await page.locator('button[class="swal2-confirm swal2-styled"]').click();
-  await page.waitForSelector(':has-text("User created successfully")');
-  await page.locator('button[class="swal2-confirm swal2-styled"]').click();
-
-});
-
-//Criação de usuário com credenciais inválidas (nome comum e senha fraca)
+      
---
+++ tests/posterstore-signup.spec.js
@@ -0,0 +1,35 @@
+// @ts-check
+
+//realizando login inválido
+const {
+    test,
+    expect
+} = require('@playwright/test');
+
+test.describe('criação de usuário', () => {
+    //login invalido com sucesso , teste finalizado 
+    test('Criando User', async ({
+        page
+    }) => {
+        await page.goto('https://poster-store.pages.dev/#');
+        await page.locator('a[class="btn btn-primary btn-sm"]').click();
+        await page.locator('input[id="swal-username"]').fill('lorelai');
+        await page.locator('input[id="swal-password"]').fill('gilmore1');
+        await page.locator('button[class="swal2-confirm swal2-styled"]').click();
+        await page.waitForSelector(':has-text("User created successfully")');
+        await page.locator('button[class="swal2-confirm swal2-styled"]').click();
+
+    });
+
+    test('Criação de usuário com credenciais inválidas', async ({
+        page
+    }) => {
+        await page.goto('https://poster-store.pages.dev/#');
+        await page.locator('a[class="btn btn-primary btn-sm"]').click();
+        await page.locator('input[id="swal-username"]').fill('jackesqueleto');
+        await page.locator('input[id="swal-password"]').fill('-');
+        await page.locator('button[class="swal2-confirm swal2-styled"]').click();
+        await page.waitForSelector(':has-text("User created successfully")');
+        await page.locator('button[class="swal2-confirm swal2-styled"]').click();
+    });
+});
