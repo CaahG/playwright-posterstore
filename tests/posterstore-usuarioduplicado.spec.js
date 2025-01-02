a82776cc742cb9d1f4aedc95967a5cc2c2f473cc
CaahG <caahg@YoungestAxes-MacBook-Air.local>
2025-01-02 17:46:59 -0300

    Subindo usuario duplicado com o mesmo endereco de username

---
+++ tests/posterstore-usuarioduplicado.spec.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+//realizando login inválido
+const {
+    test,
+    expect
+} = require('@playwright/test');
+
+test.describe('Criando usuário com 70 caractere', () => {
+    
+    test('Realizando uma compra', async ({
+        page
+    }) => {
+
+
+await page.goto('https://poster-store.pages.dev/#');
+await page.locator('a[id="signup-button"]').click();
+await page.locator('input[id="swal-username"]').fill('MickeyTestador_123_PlanetaMágico_DisneyMania_Aventuras_2025');
+await page.locator('input[id="swal-password"]').fill('Pass123456@disneycarapraumcaramba');
+await page.locator('button[class="swal2-confirm swal2-styled"]').click();
+await page.waitForSelector(':has-text("User created successfully")');
+
+
+});
+
+test('Criação de usuário duplicado', async ({
+    page
+}) => {
+    await page.goto('https://poster-store.pages.dev/#');
+    await page.locator('a[id="signup-button"]').click();
+    await page.locator('input[id="swal-username"]').fill('MickeyTestador_123_PlanetaMágico_DisneyMania_Aventuras_2025');
+    await page.locator('input[id="swal-password"]').fill('Pass123456@disneycara');
+    await page.locator('button[class="swal2-confirm swal2-styled"]').click();
+    await page.waitForSelector(':has-text("User created successfully")');
+
+});
+});
